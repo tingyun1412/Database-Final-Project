a3b5d3b0072eb6299745bc163465b41268660a10
linda46138 <linda46138@gmail.com>
2024-11-14 15:25:05 +0800

    message

---
+++ Album.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Album (
+    album_name VARCHAR(255),
+    song_id VARCHAR(50),
+    song_name VARCHAR(255),
+    singer_name VARCHAR(255),
+    PRIMARY KEY(album_name, song_id, singer_name)
+);
+
+CREATE TABLE AlbumStaging (
+    album_name VARCHAR(255),
+    song_id VARCHAR(50),
+    song_name VARCHAR(255),
+    singer_name VARCHAR(255)
+);
+
+LOAD DATA INFILE '/var/lib/mysql-files/album.csv'
+INTO TABLE AlbumStaging
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+
+(@id, @name, @dummy, @artists, @album, @dummy, @dummy, @dummy)
+SET album_name = @album, 
+    song_id = @id, 
+    song_name = @name,
+    singer_name = @artists;
+
+INSERT IGNORE INTO Album (album_name, song_id, song_name, singer_name)
+SELECT album_name, song_id, song_name, singer_name
+FROM AlbumStaging
